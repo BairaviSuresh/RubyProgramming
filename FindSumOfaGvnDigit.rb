a3c74ce107b53e2c3b3fd6a7d72077230d5e86de
BairaviSuresh <64737223+BairaviSuresh@users.noreply.github.com>
2020-05-03 21:32:40 +0530

    Digit Sum till last number

--- FindSumOfaGvnDigit.rb
+++ FindSumOfaGvnDigit.rb
@@ -9,4 +9,21 @@ def FindSumOfAGvnDigit(givenNo)
   puts "Sum of digits is #{sum}"
 end
 
-FindSumOfAGvnDigit(323409)
+FindSumOfAGvnDigit(687)
+
+
+def digitSumTillLastNo(number)
+  sum1 = 0
+  while (number != 0 || sum1 > 9)
+    if(number == 0) # When no becomes 0, assign the sum which is greater than 9
+      # to number and clear sum
+      number = sum1
+      sum1 = 0 # Clear the existing values on sum
+    end
+    sum1 = sum1 + (number % 10) # Provides the last digit
+    number = number / 10 # Provides the first digits
+  end
+  puts "Sum of digits till last digit is #{sum1}"
+end
+
+digitSumTillLastNo(687)
